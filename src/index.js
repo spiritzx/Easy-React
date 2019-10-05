b745990c0e06a9e89adcb053d84315c23ac7d3b1
tomz <spirt108@foxmail.com>
2019-10-05 15:30:13 +0800

    学习 jsx

--- src/index.js
+++ src/index.js
@@ -1,8 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './index.scss';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+import home from "./views/home/home";
+import about from "./views/about/about";
+import Header from "./components/header/header";
+import Footer from "./components/footer/footer";
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <Header></Header>
+          <div className="pageMain" id="main">
+            <Route exact path='/' component={home}></Route>
+            <Route exact path='/home' component={home}></Route>
+            <Route exact path='/about' component={about}></Route>
+          </div>
+          <Footer/>
+        </div>
+      </Router>
+    )
+  }
+}
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
--- src/views/home/home.jsx
+++ src/views/home/home.jsx
@@ -1,8 +1,29 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+let obj = {
+  name: "tom_z",
+  sex: "男",
+  arr: [
+    {item: 1},
+    {item: 2},
+    {item: 3}
+  ]
+}
+function renderFn(props) {
+  return <div>
+    <p>首页</p>
+    <div><span>名字：</span>{ props.name }</div>
+    <div><span>性别：</span>{ props.sex }</div>
+    <ul>
+      {props.arr.map(val => {
+        return <li key={val.item}>{val.item}</li>
+      })}
+    </ul>
+  </div>
+}
+
+
 export default class home extends Component {
   render() {
-    return (
-      <div>首页</div>
-    );
+    return renderFn(obj);
   }
 }
