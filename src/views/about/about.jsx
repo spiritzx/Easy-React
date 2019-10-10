6822c6c8e0ce6998ec01f54945e7eb6ac80c8a74
tomz <spirt108@foxmail.com>
2019-10-10 23:26:33 +0800

    bind event

--- src/views/about/about.jsx
+++ src/views/about/about.jsx
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
-export default class home extends Component {
+
+export default class Home extends Component {
+  constructor() {
+    super();
+    this.state = {
+      msg: "关于"
+    }
+  };
   render() {
-    return (
-      <div>关于</div>
-    );
+    return <div>
+      <p onClick={() => { this.showFn(2) }}>修改</p>
+      <h1>{this.state.msg}</h1>
+    </div>;
+  };
+  showFn = function (arg) {
+    this.setState({
+      msg: "5"
+    })
   }
 }
--- src/views/home/home.jsx
+++ src/views/home/home.jsx
@@ -1,39 +1,49 @@
 import React, { Component } from "react";
 import "./home.scss";
 
-let obj = {
-  name: "tom_z",
-  sex: "男",
-  arr: [
-    {item: 1},
-    {item: 2},
-    {item: 3}
-  ]
-}
-let _class = "title-h1";
-let style = {
-  color: "#ccc"
-}
-
-function renderFn(props) {
-  return <div>
-    <p
-      className={_class + " title"}
-      style={style}
-    >首页</p>
-    <div><span>名字：</span>{ props.name }</div>
-    <div><span>性别：</span>{ props.sex }</div>
-    <ul>
-      {props.arr.map(val => {
-        return <li key={val.item}>{val.item}</li>
-      })}
-    </ul>
-  </div>
-}
-
-
 export default class home extends Component {
+  constructor() {
+    super();
+    this.state = {
+      obj: {
+        name: "tom_z",
+        sex: "男",
+        arr: [
+          {item: 1},
+          {item: 2},
+          {item: 3}
+        ]
+      },
+      _class: "title-h1",
+      style: {
+        color: "#ccc"
+      }
+    }
+  }
   render() {
-    return renderFn(obj);
+    return this.renderFn(this.state.obj);
+  };
+  renderFn = function (props) {
+    return <div>
+      <p
+        className={this.state._class + " title"}
+        style={this.state.style}
+      >首页</p>
+      <button onClick={() => {this.setName(1)}}>设置名字</button>
+      <button onClick={() => {this.setSex(2)}}>设置性别</button>
+      <div><span>名字：</span>{ props.name }</div>
+      <div><span>性别：</span>{ props.sex }</div>
+      <ul>
+        {props.arr.map(val => {
+          return <li key={val.item}>{val.item}</li>
+        })}
+      </ul>
+    </div>
+  };
+  setName = function(arg) {
+    console.log(arg)
+  };
+  setSex = function(arg) {
+    console.log(arg)
   }
 }
