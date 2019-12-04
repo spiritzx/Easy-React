39c0d456a14c7328aa7792998d3698d3fc82cb07
tomz <spirt108@foxmail.com>
2019-12-05 00:00:36 +0800

    other

--- src/components/routerComp/routerComp.jsx
+++ src/components/routerComp/routerComp.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
-
 import home from "../../views/home/home";
 import about from "../../views/about/about";
 import "./style/routerComp.scss";
